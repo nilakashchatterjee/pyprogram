e1ddaff9f7c4340f1098805b3509bbdc79533512
nilakashchatterjee <work.nilakash@gmail.com>
2021-11-10 00:47:49 +0530

    first

--- sample.py
+++ sample.py
@@ -55,12 +55,3 @@
 #     print(i)
 
 
-def scope_test():
-    def do_local():
-        spam="local spam"
-    def do_nonlocal():
-        nonlocal spam
-        spam= "nonlocal spam"
-    def do_global():
-        global spam
-        spam="global spam"
