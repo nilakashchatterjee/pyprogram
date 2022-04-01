837427ac3e50217a34dfdd302dcc746ca0d414f2
nilakashchatterjee <work.nilakash@gmail.com>
2022-04-01 17:44:21 +0530

    sample

--- sample.py
+++ sample.py
@@ -1,57 +1,44 @@
-# def funct1(obj,index):
-#     print("value at position {} is {}".format(index,obj[index]))
-# l=[10,20,25,30,23]
-# try:
-#     funct1(l,2)
-# except IndexError:
-#     print("Got exception")
-# print("End of the program")
+def prefixmatch(str1, str2):
+ 
+    result = "";
+    n1 = len(str1)
+    n2 = len(str2)
+ 
+    # Compare str1 and str2
+    i = 0
+    j = 0
+    while i <= n1 - 1 and j <= n2 - 1:
+     
+        if (str1[i] != str2[j]):
+            break
+             
+        result += str1[i]
+        i += 1
+        j += 1
+ 
+    return (result)
+ 
 
-# # raising exception
-# try:
-#     print("raise exception")
-#     raise abc
-# except IndexError:g
-#     print("caught exception")
-# lst=[2,3,4,5,6,7,8]
-# print(lst.index(8))
-# import math as m
-# print(math.sqrt(81))
-# s=set('abc')
-# s.add('mno')
-# s.update(set(['p','q']))
-# print(s)
-# i=9
-# j=7
-
-
-# class  A:
-#     def __init__(self):
-#         self.mul(15)
-        
-#     def mul(self,i):
-#         self.i=4*i
-
-# class B(A):
-#     def __init__(self):
-#         super().__init__()
-#         print(self.i)
-#     def mul(self,i):
-#         self.i=2*i
-# obj=B()
-
-        
-# class error(Exception):
-#     def __init__(self,num):
-#         self.num=num
-# try:
-#     raise error(123)
-# except error as e:
-#     print("reciebed error",e.num)
-
-# for i in range(5):
-#     if i==3:
-#         break
-#     print(i)
-
-
+def commonPrefix (arr, n):
+ 
+    prefix = arr[0]
+ 
+    for i in range (1, n):
+        prefix = prefixmatch(prefix, arr[i])
+ 
+    return (prefix)
+ 
+# Driver Code
+if __name__ =="__main__":
+ 
+    arr = ["flower","flow","flight"]
+    # arr = ["dog","race","car"]
+    n = len(arr)
+ 
+    ans = commonPrefix(arr, n)
+ 
+    if (len(ans)):
+        print ("The longest common prefix is -",
+                ans);
+    else:
+        print("There is no common prefix")
